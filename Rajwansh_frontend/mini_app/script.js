25ebda7528560802b017c73f574c4109f5dbea15
RajwanshBhati <rajwanshbhati@gmail.com>
2026-03-22 12:29:21 +0530

    feat: add Product data in JS file

---
+++ Rajwansh_frontend/mini_app/script.js
@@ -0,0 +1,10 @@
+let products = [
+  { id: 1, name: "Laptop", price: 55000, stock: 5, category: "electronics" },
+  { id: 2, name: "Smartphone", price: 20000, stock: 10, category: "electronics" },
+  { id: 3, name: "Headphones", price: 2000, stock: 2, category: "accessories" },
+  { id: 4, name: "T-Shirt", price: 800, stock: 15, category: "clothing" },
+  { id: 5, name: "Jeans", price: 1500, stock: 0, category: "clothing" },
+  { id: 6, name: "Book - JS Guide", price: 500, stock: 8, category: "books" },
+  { id: 7, name: "Backpack", price: 1200, stock: 3, category: "accessories" },
+  { id: 8, name: "Tablet", price: 30000, stock: 4, category: "electronics" }
+];
