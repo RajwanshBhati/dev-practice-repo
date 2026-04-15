a87f00c15482c7c3a4880e48308d706c8b4cc9d8
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-16 00:05:09 +0530

    feat: added the SubmitRequest dto

---
+++ Rajwansh_java_training/session3/src/main/java/com/springrest/restapi/dto/SubmitRequest.java
@@ -0,0 +1,24 @@
+package com.springrest.restapi.dto;
+
+
+public class SubmitRequest {
+
+    private String title;
+    private String description;
+    private String category;
+    private String submittedBy;
+
+    public SubmitRequest() {}
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public String getSubmittedBy() { return submittedBy; }
+    public void setSubmittedBy(String submittedBy) { this.submittedBy = submittedBy; }
+}
