49624bfc3125a3fdf0787c9b282e633a670db60e
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-18 14:35:35 +0530

    Create TodoService interface with CRUD method definitions

--- Rajwansh_java_training/session4/src/main/java/com/todoapp/springboot/repository/TodoRepository.java
+++ Rajwansh_java_training/session4/src/main/java/com/todoapp/springboot/repository/TodoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-
 @Repository// Here I define the TodoRepository interface, which extends JpaRepository. This interface provides CRUD operations for the Todo Entity.
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     
