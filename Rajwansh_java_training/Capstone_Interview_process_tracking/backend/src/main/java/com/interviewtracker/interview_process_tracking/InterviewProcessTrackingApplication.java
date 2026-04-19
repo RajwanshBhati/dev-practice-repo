d102bd0abad2413c54e7c4e52d1c14b7b44aeef7
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-19 22:58:54 +0530

    chore: add spring boot dependencies and postgresql setup

---
+++ Rajwansh_java_training/Capstone_Interview_process_tracking/backend/src/main/java/com/interviewtracker/interview_process_tracking/InterviewProcessTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.interviewtracker.interview_process_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewProcessTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewProcessTrackingApplication.class, args);
+	}
+
+}
