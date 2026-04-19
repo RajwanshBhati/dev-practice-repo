3fa75741c456f621b417c90a9c694f022f00677e
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-19 23:21:18 +0530

    feat: implement the JDResponse DTO

---
+++ Rajwansh_java_training/Capstone_Interview_process_tracking/backend/src/main/java/com/interviewtracker/interview_process_tracking/dto/JDResponseDTO.java
@@ -0,0 +1,30 @@
+package com.interviewtracker.interview_process_tracking.dto;
+
+import com.interviewtracker.interview_process_tracking.enum.JDStatus;
+import com.interviewtracker.interview_process_tracking.enum.JobType;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JDResponseDTO {
+
+    private UUID id;
+    private String jobTitle;
+    private String jobDescription;
+    private List<String> skillsRequired;
+    private Integer experience;
+    private BigDecimal salary;
+    private String location;
+    private JobType jobType;
+    private JDStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
