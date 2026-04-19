ae530db410b9a8591feb6a6b9a6a9cc86037c816
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-19 13:03:42 +0530

    feat:Write a unit testing

--- Rajwansh_java_training/session4/src/test/java/com/todoapp/springboot/SpringbootApplicationTests.java
+++ Rajwansh_java_training/session4/src/test/java/com/todoapp/springboot/SpringbootApplicationTests.java
@@ -2,6 +2,7 @@ package com.todoapp.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class SpringbootApplicationTests {
@@ -9,5 +10,12 @@ class SpringbootApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+    void mainMethod_shouldRunWithoutException() {
+        assertDoesNotThrow(() -> {
+            SpringbootApplication.main(new String[]{});
+        });
+    }
 
 }
