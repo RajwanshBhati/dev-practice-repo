d56986243382b9a62201c056f94095e47a35dc57
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-19 23:08:40 +0530

    feat:created two enum for JD

---
+++ Rajwansh_java_training/Capstone_Interview_process_tracking/backend/src/main/java/com/interviewtracker/interview_process_tracking/enum/JDStatus.java
@@ -0,0 +1,7 @@
+package com.interviewtracker.interview_process_tracking.enum;
+
+public enum JDStatus {
+    ACTIVE,
+    INACTIVE,
+    CLOSED
+}
---
+++ Rajwansh_java_training/Capstone_Interview_process_tracking/backend/src/main/java/com/interviewtracker/interview_process_tracking/enum/JobType.java
@@ -0,0 +1,7 @@
+package com.interviewtracker.interview_process_tracking.enum;
+
+public enum JobType {
+    FULL_TIME,
+    CONTRACT,
+    REMOTE
+}
