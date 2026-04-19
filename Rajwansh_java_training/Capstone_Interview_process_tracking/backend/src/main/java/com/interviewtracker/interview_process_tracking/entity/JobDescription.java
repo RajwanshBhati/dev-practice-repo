09060f9ca055a148278bb020fe300bbcd1e4484e
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-19 23:16:37 +0530

    feat: enhance job description entity with audit fields and enum support

---
+++ Rajwansh_java_training/Capstone_Interview_process_tracking/backend/src/main/java/com/interviewtracker/interview_process_tracking/entity/JobDescription.java
@@ -0,0 +1,65 @@
+package com.ats.jobdescription.entity;
+
+import com.ats.jobdescription.enums.JDStatus;
+import com.ats.jobdescription.enums.JobType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "job_descriptions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobDescription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "job_title", nullable = false)
+    private String jobTitle;
+
+    @Column(name = "job_description", nullable = false, columnDefinition = "TEXT")
+    private String jobDescription;
+
+    @ElementCollection
+    @CollectionTable(name = "jd_skills", joinColumns = @JoinColumn(name = "jd_id"))
+    @Column(name = "skill")
+    private List<String> skillsRequired;
+
+    @Column(name = "experience", nullable = false)
+    private Integer Experience;        // in years
+
+    @Column(name = "salary", nullable = false, precision = 12, scale = 2)
+    private BigDecimal Salary;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "job_type", nullable = false)
+    private JobType jobType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    @Builder.Default
+    private JDStatus status = JDStatus.ACTIVE;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
