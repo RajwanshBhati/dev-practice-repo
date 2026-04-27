fe674a9e0564e8c9077624cd47c5f441e025a994
RajwanshBhati <rajwanshbhati@gmail.com>
2026-04-27 22:04:14 +0530

    feat:added the Interview Repo

---
+++ Rajwansh_java_training/Capstone_Interview_Tracking_System/backend/src/main/java/com/interview_tracking_system/backend/repository/InterviewRepository.java
@@ -0,0 +1,42 @@
+package com.interview_tracking_system.backend.repository;
+
+import com.interview_tracking_system.backend.entity.Interview;
+import com.interview_tracking_system.backend.enums.Stage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Repository for Interview entity.
+ */
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+
+    Logger LOGGER = LoggerFactory.getLogger(InterviewRepository.class);
+
+    /**
+     * Fetch all interviews for a candidate.
+     *
+     * @param candidateId candidate id
+     * @return list of interviews
+     */
+    List<Interview> findByCandidateId(Long candidateId);
+
+    /**
+     * Fetch interviews by panel id.
+     *
+     * @param panelId panel id
+     * @return list of interviews
+     */
+    List<Interview> findByPanelId(Long panelId);
+
+    /**
+     * Fetch interviews by stage (L1 / L2).
+     *
+     * @param stage stage
+     * @return list of interviews
+     */
+    List<Interview> findByStage(Stage stage);
+}
