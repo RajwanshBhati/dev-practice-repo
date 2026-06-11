4d57b940b56f9866040d2cb9c6c6f2f07a9fc3c7
RajwanshBhati <rajwanshbhati@gmail.com>
2026-06-12 01:26:58 +0530

    feat:added operator and conditional program

---
+++ Rajwansh_python_training/python_basic/operators_and_conditionals/grade_calculator.py
@@ -0,0 +1,12 @@
+# Take marks as input
+marks = int(input("Enter marks: "))
+
+# Check grade using conditional statements
+if marks >= 80:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 40:
+    print("Grade C")
+else:
+    print("Fail")
---
+++ Rajwansh_python_training/python_basic/operators_and_conditionals/positive_negative_zero.py
@@ -0,0 +1,14 @@
+# Take a number from the user
+num = float(input("Enter a number: "))
+
+# Check if the number is positive
+if num > 0:
+    print("Positive Number")
+
+# Check if the number is negative
+elif num < 0:
+    print("Negative Number")
+
+# If neither positive nor negative, it is zero
+else:
+    print("Zero")
