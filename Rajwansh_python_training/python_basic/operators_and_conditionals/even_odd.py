5b9f94ea4c01023f717de339363d73a6037a2780
RajwanshBhati <rajwanshbhati@gmail.com>
2026-06-12 01:13:33 +0530

    feat: Implement the arithmetic operation and even odd program

---
+++ Rajwansh_python_training/python_basic/operators_and_conditionals/even_odd.py
@@ -0,0 +1,8 @@
+# input() is used to take a number from the user
+num = int(input("Enter a number: "))
+
+# % (modulus) returns the remainder after division
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
---
+++ Rajwansh_python_training/python_basic/variables_and_data_types/arithmetic_operations.py
@@ -0,0 +1,9 @@
+# input() is used to take numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Arithmetic operators are used to perform mathematical calculations
+print("Sum =", num1 + num2)
+print("Difference =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
