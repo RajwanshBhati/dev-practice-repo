598560e91aa95ef4391b3131acab42190353d278
RajwanshBhati <rajwanshbhati@gmail.com>
2026-06-20 11:19:00 +0530

    feat: add employee dataset constants

---
+++ Rajwansh_python_training/python_advance/assignment02/data_analysis/constants.py
@@ -0,0 +1,38 @@
+"""
+Constants for Data Analysis assignment.
+
+Constants are stored separately to avoid hardcoded values.
+"""
+
+EMPLOYEE_DATA = {
+    "Name": [
+        "Rahul",
+        "Priya",
+        "Amit",
+        "Anuj",
+    ],
+    "Age": [
+        25,
+        30,
+        28,
+        35,
+    ],
+    "Department": [
+        "HR",
+        "IT",
+        "Finance",
+        "IT",
+    ],
+    "Salary": [
+        30000,
+        50000,
+        45000,
+        60000,
+    ],
+}
+
+DEPARTMENT_COLUMN_NAME = "Department"
+
+SALARY_COLUMN_NAME = "Salary"
+
+NAME_COLUMN_NAME = "Name"
