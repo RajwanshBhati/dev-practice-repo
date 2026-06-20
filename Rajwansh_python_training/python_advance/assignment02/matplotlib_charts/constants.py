ba1e11a931a4eb21c52627e7f6efc40192114eba
RajwanshBhati <rajwanshbhati@gmail.com>
2026-06-20 12:15:16 +0530

    feat: add chart constant

---
+++ Rajwansh_python_training/python_advance/assignment02/matplotlib_charts/constants.py
@@ -0,0 +1,43 @@
+"""
+Constants for Matplotlib assignment.
+
+Constants are stored separately to avoid hardcoded values.
+"""
+
+DEPARTMENTS = [
+    "HR",
+    "IT",
+    "Finance",
+]
+
+EMPLOYEE_COUNTS = [
+    5,
+    12,
+    7,
+]
+
+SALARY_VALUES = [
+    30000,
+    40000,
+    50000,
+    60000,
+    45000,
+]
+
+AGE_VALUES = [
+    25,
+    30,
+    28,
+    35,
+    29,
+]
+
+BAR_CHART_TITLE = "Employees By Department"
+
+LINE_CHART_TITLE = "Department Employee Trend"
+
+HISTOGRAM_TITLE = "Salary Distribution"
+
+SCATTER_PLOT_TITLE = "Age vs Salary"
+
+NUMBER_OF_HISTOGRAM_BINS = 5
