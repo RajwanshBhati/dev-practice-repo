55b53091846c1bc8c783a8bb0f4f5d4ba94eeba2
RajwanshBhati <rajwanshbhati@gmail.com>
2026-06-20 12:58:25 +0530

    feat:added the constants file

---
+++ Rajwansh_python_training/python_advance/assignment02/mini_project/constants.py
@@ -0,0 +1,44 @@
+"""
+Constants for Student Performance Mini Project.
+"""
+
+STUDENT_DATA = {
+    "Name": [
+        "Rahul",
+        "Priya",
+        "Siri",
+        "Anuj",
+    ],
+    "Marks": [
+        70,
+        80,
+        90,
+        60,
+    ],
+    "Hours Studied": [
+        2,
+        3,
+        5,
+        1,
+    ],
+}
+
+NAME_COLUMN_NAME = "Name"
+
+MARKS_COLUMN_NAME = "Marks"
+
+HOURS_STUDIED_COLUMN_NAME = "Hours Studied"
+
+PERFORMANCE_COLUMN_NAME = "Performance"
+
+PASS_STATUS = "Pass"
+
+FAIL_STATUS = "Fail"
+
+PASSING_MARKS = 65
+
+LINE_CHART_TITLE = "Hours Studied vs Marks"
+
+SCATTER_PLOT_TITLE = "Study Hours vs Marks"
+
+BARPLOT_TITLE = "Performance vs Marks"
